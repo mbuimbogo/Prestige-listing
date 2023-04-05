3171e27e9a8639ed3236f453de297e01b5b4e0c8
mbuimbogo <mbuimbogo99@gmail.com>
2023-04-05 12:10:43 +0300

    Add websites logo, add mximum header width and hovering effect

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,34 @@
-import {AiFillHome} from "react-icons/ai"
-import {BsFillInfoCircleFill} from "react-icons/bs"
-import Link from 'next/link'
-import DarkModeSwitch from './DarkModeSwitch'
-import MenuItem from "./MenuItem"
+import { AiFillHome } from "react-icons/ai";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+import Link from "next/link";
+import DarkModeSwitch from "./DarkModeSwitch";
+import MenuItem from "./MenuItem";
 
 export default function Header() {
   return (
-    <div className='flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6'>
-      <div className="flex">
-        <MenuItem title="HOME" address="/" Icon={AiFillHome}/>
-        <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill}/>
+    <div className="flex justify-between mx-2 max-w-7xl sm:mx-auto items-center py-6 bg-gray-200 sticky top-0 z-30">
+      <div className="sm:ml-3">
+        <img
+          src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
+          alt="Logo"
+          className="h-5 cursor-pointer"
+        />
       </div>
+      <div className="flex">
+        <MenuItem title="HOME" address="/" Icon={AiFillHome} />
+        <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill} />
+      </div>
+
       <div className="flex items-center space-x-5">
-        <DarkModeSwitch/>
-        <Link href="/">
-            <h2 className='text-2xl'><span className='font-bold bg-orange-500 py-1 px-2 rounded-lg mr-1'>Prestige</span><span className='text-xl hidden sm:inline'>Properties</span></h2>
+        <DarkModeSwitch />
+        <Link href="/about">
+          <h2 className="text-2xl">
+            <span className="font-bold bg-red-300 py-1 px-2 rounded-lg mr-4">
+              Prestige Homes
+            </span>
+          </h2>
         </Link>
       </div>
     </div>
-  )
+  );
 }
--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function MenuItem({title, address, Icon}) {
   return (
     <div>
-      <Link href={address} className="mx-4 lg:mx-6 hover:text-amber-600">
+      <Link href={address} className="mx-4 lg:mx-6 hover:text-amber-600 hover:bg-red-200 p-1">
         <Icon className="text-2xl sm:hidden mx-4"/>
       <p className='hidden sm:inline my-2 text-sm'>{title}</p>
       </Link>
