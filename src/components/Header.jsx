e5f6ebf908128da9ea3c4751e94d8551c1890039
mbuimbogo <mbuimbogo99@gmail.com>
2023-04-05 04:05:18 +0300

    Create a responsive header

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import {AiFillHome} from "react-icons/ai"
+import {BsFillInfoCircleFill} from "react-icons/bs"
+import Link from 'next/link'
+// import DarkModeSwitch from './DarkModeSwitch'
+import MenuItem from "./MenuItem"
+
+export default function Header() {
+  return (
+    <div className='flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6'>
+      <div className="flex">
+        <MenuItem title="HOME" address="/" Icon={AiFillHome}/>
+        <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill}/>
+      </div>
+      <div className="flex items-center space-x-5">
+        {/* <DarkModeSwitch/> */}
+        <Link href="/">
+            <h2 className='text-2xl'><span className='font-bold bg-orange-500 py-1 px-2 rounded-lg mr-1'>Prestige</span><span className='text-xl hidden sm:inline'>Properties</span></h2>
+        </Link>
+      </div>
+    </div>
+  )
+}
