8ce8c59ad93538a247417f168c62db2198bb056c
mbuimbogo <mbuimbogo99@gmail.com>
2023-04-05 04:38:58 +0300

    Handle errors when fetching

---
+++ src/app/error.jsx
@@ -0,0 +1,15 @@
+"use client"
+import React from 'react'
+import { useEffect } from 'react'
+
+export default function Error({ error,reset }) {
+    useEffect(()=>{
+        console.log(error)
+    },[error])
+  return (
+    <div className='text-center mt-10'>
+      <h1>Something is a miss</h1>
+      <button  className="hover:text-red-600" onClick={()=> reset()}>Try again</button>
+    </div>
+  )
+}
